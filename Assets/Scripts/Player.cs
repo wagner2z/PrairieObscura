73f1d274034d357f65af4c5efab6bf8c22c14199
wagner2z <46720430+wagner2z@users.noreply.github.com>
2025-10-17 23:23:31 -0600

    Initial Unity Project Commit

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 mouseWorldPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        mouseWorldPos.z = 0;
+        Vector3 direction = mouseWorldPos - transform.position;
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        // Adjust for sprite's initial orientation if necessary
+        angle -= 90; // Example adjustment
+        transform.rotation = Quaternion.Euler(0, 0, angle);
+    }
+}
